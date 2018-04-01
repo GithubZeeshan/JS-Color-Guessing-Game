3e4277a057678ed6d86c6775f445bcaba3d9b778
GithubZeeshan <ZeeshanKhanDeveloper@gmail.com>
2018-04-01 15:25:24 +0800

    V 0.2
    - Fixed layout issues
    - Added difficulty levels
    - Added transitions

--- ColorGuessingGame.js
+++ ColorGuessingGame.js
@@ -1,36 +1,81 @@
+//Variables
 var colorBoxList = document.querySelectorAll(".colorBox");
 var colorDisplay = document.getElementById("RGB");
 var choiceFeedback = document.getElementById("correctOrWrong");
+var playAgain = document.getElementById("playAgain");
+var gameHeader = document.getElementById("topHeader");
+var gameMode = document.querySelectorAll(".gameMode");
+var gameComplete = false;
+//Variables end
 
-function generateColors() {
+//Functions
+function generateColors(numberToGenerate) {
 	
-	for (var i = 0; i < colorBoxList.length; i++) {
+	for (var i = 0; i < numberToGenerate; i++) {
 		var r = Math.floor(Math.random() * 255);
 		var g = Math.floor(Math.random() * 255);
 		var b = Math.floor(Math.random() * 255);
-		var rgb = "rgb(" + r + "," + g + "," + b + ")";
+		var rgb = "RGB(" + r + "," + g + "," + b + ")";
 		colorBoxList[i].style.backgroundColor = rgb;
-		console.log(rgb);
 	}
 }
 
-function pickRandomOption() {
-	var randomChoice = Math.floor(Math.random() * 6);
+function pickRandomOption(maxNumber) {
+	var randomChoice = Math.floor(Math.random() * maxNumber);
 	colorDisplay.textContent = colorBoxList[randomChoice].style.backgroundColor;
 }
 
-generateColors();
-pickRandomOption();
+function setupGame(numColorBoxes) {
+	generateColors(numColorBoxes);
+	pickRandomOption(numColorBoxes);
+	gameHeader.style.backgroundColor = "rgb(33, 137, 255)";
+	choiceFeedback.textContent = "Select a color box";
+	playAgain.textContent = "";
+	gameComplete = false;
 
+	for (var i = 3; i < colorBoxList.length; i++){
+		if (numColorBoxes === 3){
+			colorBoxList[i].classList.add("hide");
+		}
+		else{
+			colorBoxList[i].classList.remove("hide");
+		}
+	}
+}
+
+function correctChoice(color) {
+	choiceFeedback.textContent = "Correct!";
+	playAgain.textContent = "Play Again?";
+	gameHeader.style.backgroundColor = color;
+	playAgain.addEventListener("click", function() {
+		gameMode[0].classList.contains("selected") ? setupGame(3) : setupGame(6);
+	});
+}
+//Functions end
+
+//Click listeners for color boxes
 for (var i = 0; i < colorBoxList.length; i++){
 	colorBoxList[i].addEventListener("click", function() {
 		var selectedColor = this.style.backgroundColor;
 		if (selectedColor === colorDisplay.textContent){
-			choiceFeedback.textContent = "Correct!";
+			gameComplete = true;
+			correctChoice(this.style.backgroundColor);
 		}
-		else{
+		else if (!gameComplete){
 			choiceFeedback.textContent = "Wrong! Try Again!";
 			this.style.backgroundColor = "rgb(35,35,35)";
 		}
 	})
 }
+
+//Click listeners for game modes
+for (var i = 0; i < gameMode.length; i++){
+	gameMode[i].addEventListener("click", function() {
+	gameMode[0].classList.remove("selected");
+	gameMode[1].classList.remove("selected");
+	this.classList.add("selected");
+	this.textContent === "EASY" ? setupGame(3) : setupGame(6);
+	});
+}
+
+setupGame(6);
